5ed0b89b02dc579854a8be885d0cb375171b4c1e
ThierryO <Thierry.Onkelinx@inbo.be>
2016-07-06 17:50:18 +0200

    store_datasource() writes the connect_method

--- R/store_datasource.R
+++ R/store_datasource.R
@@ -2,11 +2,32 @@
 #' @param datasource a data.frame with datasource metadata
 #' @param conn a DBIconnection
 #' @export
-#' @importFrom assertthat assert_that
-#' @details datasource must contain the variable
+#' @importFrom assertthat assert_that has_name
+#' @importFrom digest sha1
+#' @importFrom dplyr %>% transmute_ distinct_
+#' @importFrom DBI dbWriteTable
+#' @details datasource must contain at least the variables description, datasource_type and connect_method.
 store_datasource <- function(datasource, conn){
   assert_that(inherits(datasource, "data.frame"))
   assert_that(inherits(conn, "DBIConnection"))
 
+  assert_that(has_name(datasource, "description"))
+  assert_that(has_name(datasource, "datasource_type"))
+  assert_that(has_name(datasource, "connect_method"))
+
+  hash <- sha1(datasource)
+
+  datasource %>%
+    transmute_(
+      ~connect_method,
+      hash = ~hash
+    ) %>%
+    distinct_() %>%
+    dbWriteTable(
+      conn = conn,
+      name = c("staging", "connect_method"),
+      append = TRUE,
+      row.names = FALSE
+    )
   return(TRUE)
 }
--- tests/testthat/test_aa_store_datasource.R
+++ tests/testthat/test_aa_store_datasource.R
@@ -1,4 +1,6 @@
 context("store_datasource")
+require(dplyr, quietly = TRUE)
+require(DBI, quietly = TRUE)
 ut.datasource <- data.frame(
   description = c("Unit test datasource 1", "Unit test datasource 2"),
   datasource_type = "PostgreSQL",
@@ -16,4 +18,35 @@ test_that("input is suitable", {
     store_datasource(datasource = ut.datasource, "junk"),
     "conn does not inherit from class DBIConnection"
   )
+  conn <- n2khelper::connect_result(
+    username = Sys.getenv("N2KRESULT_USERNAME"),
+    password = Sys.getenv("N2KRESULT_PASSWORD")
+  )$con
+  expect_error(
+    ut.datasource %>%
+      select_(~-description) %>%
+      store_datasource(conn),
+    "datasource does not have name description"
+  )
+  expect_error(
+    ut.datasource %>%
+      select_(~-datasource_type) %>%
+      store_datasource(conn),
+    "datasource does not have name datasource_type"
+  )
+  expect_error(
+    ut.datasource %>%
+      select_(~-connect_method) %>%
+      store_datasource(conn),
+    "datasource does not have name connect_method"
+  )
+  dbDisconnect(conn)
+})
+test_that("it writes the correct data to the staging tables", {
+  conn <- n2khelper::connect_result(
+    username = Sys.getenv("N2KRESULT_USERNAME"),
+    password = Sys.getenv("N2KRESULT_PASSWORD")
+  )$con
+  expect_true(store_datasource(datasource = ut.datasource, conn = conn))
+  dbDisconnect(conn)
 })
