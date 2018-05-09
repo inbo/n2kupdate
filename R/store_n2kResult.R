6d69c355b5aca659f74d410cbe2b1695506ce214
ThierryO <Thierry.Onkelinx@inbo.be>
2018-05-09 11:08:43 +0200

    add store_n2kResult()

---
+++ R/store_n2kResult.R
@@ -0,0 +1,178 @@
+#' store an n2kResult object into the database
+#' @inheritParams store_datasource_parameter
+#' @param object a \code{\link[n2kanalysis]{n2kResult-class}} object
+#' @importFrom methods validObject
+#' @importFrom digest sha1
+#' @importFrom assertthat assert_that is.string is.flag noNA
+#' @importFrom DBI dbBegin dbSendQuery dbRemoveTable dbCommit dbClearResult dbQuoteIdentifier dbWriteTable
+#' @importFrom dplyr %>% distinct inner_join select
+#' @importFrom rlang .data
+#' @importFrom tibble rowid_to_column
+store_n2kResult <- function(object, conn, hash, clean = TRUE) {
+  assert_that(inherits(object, "n2kResult"))
+  validObject(object)
+  stopifnot(nrow(object@Contrast) == 0)
+
+  if (missing(hash)) {
+    hash <- sha1(
+      list(
+        object@AnalysisMetadata, object@AnalysisRelation,
+        object@AnalysisVersion, object@RPackage, object@AnalysisVersionRPackage,
+        object@AnomalyType, object@Anomaly, object@Parameter,
+        object@ParameterEstimate, object@Contrast, object@ContrastCoefficient,
+        object@ContrastEstimate, as.POSIXct(Sys.time())
+      )
+    )
+  } else {
+    assert_that(is.string(hash))
+  }
+
+  assert_that(is.flag(clean))
+  assert_that(noNA(clean))
+
+  if (clean) {
+    dbBegin(conn)
+  }
+
+  object@AnalysisMetadata %>%
+    distinct(
+      description = .data$ModelType,
+      first_year = .data$FirstImportedYear,
+      last_year = .data$LastImportedYear,
+      duration = .data$Duration
+    ) %>%
+    rowid_to_column("local_id") -> model_set
+  object@AnalysisMetadata %>%
+    inner_join(
+      model_set,
+      by = c(
+        "ModelType" = "description",
+        "FirstImportedYear" = "first_year",
+        "LastImportedYear" = "last_year",
+        "Duration" = "duration"
+      )
+    ) %>%
+    select(
+      file_fingerprint = .data$FileFingerprint,
+      model_set_local_id = .data$local_id,
+      location_group = .data$LocationGroupID,
+      species_group = .data$SpeciesGroupID,
+      last_year = .data$LastAnalysedYear,
+      seed = .data$Seed,
+      analysis_version = .data$AnalysisVersion,
+      analysis_date = .data$AnalysisDate,
+      status = .data$Status,
+      status_fingerprint = .data$StatusFingerprint
+    ) -> analysis
+  object@AnalysisRelation %>%
+    select(
+      analysis = .data$Analysis,
+      source_analysis = .data$ParentAnalysis
+    ) -> analysis_relation
+  tryCatch(
+    store_analysis(
+      analysis = analysis,
+      model_set = model_set,
+      analysis_version = object,
+      analysis_relation = analysis_relation,
+      conn = conn,
+      hash = hash,
+      clean = FALSE
+    ),
+    error = function(e){
+      if (clean) {
+        dbRollback(conn)
+      }
+      stop(e)
+    }
+  )
+  object@Parameter %>%
+    select(
+      local_id = .data$Fingerprint,
+      parent_parameter_local_id = .data$Parent,
+      description = .data$Description
+    ) -> parameter
+  object@AnomalyType %>%
+    select(local_id = .data$Fingerprint, description = .data$Description) ->
+    anomaly_type
+  tryCatch(
+    object@Anomaly %>%
+      select(
+        anomaly_type_local_id = .data$AnomalyType,
+        parameter_local_id = .data$Parameter,
+        analysis = .data$Analysis,
+        observation = .data$Observation
+      ) %>%
+      store_anomaly(
+        anomaly_type = anomaly_type,
+        parameter = parameter,
+        hash = hash,
+        conn = conn,
+        clean = FALSE
+      ) -> anomaly,
+    error = function(e){
+      if (clean) {
+        dbRollback(conn)
+      }
+      stop(e)
+    }
+  )
+  object@ParameterEstimate %>%
+    rename(
+      analysis = .data$Analysis,
+      parameter = .data$Parameter,
+      estimate = .data$Estimate,
+      lcl = .data$LowerConfidenceLimit,
+      ucl = .data$UpperConfidenceLimit
+    ) %>%
+    as.data.frame() %>%
+    dbWriteTable(
+      conn = conn,
+      name = c("staging", paste0("parameter_estimate_", hash)),
+      row.names = FALSE
+    )
+  parameter_estimate.sql <- paste0("parameter_estimate_", hash) %>%
+    dbQuoteIdentifier(conn = conn)
+  tryCatch(
+    sprintf("
+      INSERT INTO public.parameter_estimate
+        (parameter, analysis, estimate, lcl, ucl)
+      SELECT p.id, a.id, s.estimate, s.lcl, s.ucl
+      FROM staging.%s AS s
+      INNER JOIN public.parameter AS p ON s.parameter = p.fingerprint
+      INNER JOIN public.analysis AS a ON s.analysis = a.file_fingerprint
+      LEFT JOIN
+        public.parameter_estimate AS pe
+      ON
+        a.id = pe.analysis AND p.id = pe.parameter
+      WHERE pe.parameter IS NULL
+      ",
+      parameter_estimate.sql
+    ) %>%
+      dbSendQuery(conn = conn) %>%
+      dbClearResult(),
+    error = function(e){
+      if (clean) {
+        dbRollback(conn)
+      }
+      stop(e)
+    }
+  )
+
+  if (clean) {
+    dbRemoveTable(conn, c("staging", paste0("analysis_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("analysis_relation_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("analysis_version_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("avrp_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("model_set_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("model_type_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("r_package_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("status_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("parameter_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("parameter_estimate_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("anomaly_", hash)))
+    dbRemoveTable(conn, c("staging", paste0("anomaly_type_", hash)))
+    dbCommit(conn)
+  }
+  return(hash)
+}
---
+++ tests/testthat/test_ce_store_n2kresult.R
@@ -0,0 +1,89 @@
+if (
+  requireNamespace("n2kanalysis", quietly = TRUE) &&
+    requireNamespace("optimx", quietly = TRUE)
+) {
+  conn <- connect_ut_db()
+  dbGetQuery(conn = conn, "SELECT fingerprint FROM scheme") %>%
+    dplyr::pull("fingerprint") %>%
+    head(1) -> this_scheme_id
+  dbGetQuery(conn = conn, "SELECT fingerprint FROM species_group") %>%
+    dplyr::pull("fingerprint") %>%
+    head(1) -> this_species_group_id
+  dbGetQuery(conn = conn, "SELECT fingerprint FROM location_group") %>%
+    dplyr::pull("fingerprint") %>%
+    head(1) -> this_location_group_id
+  dbGetQuery(conn = conn, "SELECT fingerprint FROM datasource") %>%
+    dplyr::pull("fingerprint") %>%
+    head(1) -> this_datasource_id
+  dbGetQuery(conn = conn, "SELECT fingerprint FROM datafield") %>%
+    dplyr::pull("fingerprint") %>%
+    head(1) -> this_field_id
+  datafield <- data.frame(
+    local_id = 1,
+    datasource = this_datasource_id,
+    table_name = "Seatbelts",
+    primary_key = "ID",
+    datafield_type = "integer",
+    stringsAsFactors = FALSE
+  )
+  dbGetQuery(conn = conn, "
+    SELECT
+      id AS local_id,
+      description,
+      parent_location AS parent_local_id,
+      external_code
+    FROM location
+    WHERE parent_location IS NULL"
+  ) %>%
+    head(1) %>%
+    mutate(datafield_local_id = datafield$local_id) -> location
+  dbGetQuery(conn = conn, "
+    SELECT
+      id as local_id, description, parent_parameter AS parent_parameter_local_id
+    FROM parameter
+    WHERE parent_parameter IS NULL
+  ") %>%
+    head(1) -> parameter
+  data("Seatbelts")
+  Seatbelts %>%
+    as.data.frame() %>%
+    mutate(
+      DatasourceID = sha1(this_datasource_id),
+      local_id = seq_len(nrow(Seatbelts)),
+      year = ceiling(local_id / 12)
+    ) -> Seatbelts
+  store_observation(
+    conn = conn,
+    datafield = datafield,
+    observation = Seatbelts %>%
+      select(.data$local_id, .data$year) %>%
+      mutate(
+        datafield_local_id = datafield$local_id,
+        external_code = .data$local_id,
+        location_local_id = location$local_id,
+        parameter_local_id = parameter$local_id
+      ),
+    location = location,
+    parameter = parameter
+  ) %>%
+    rename(ObservationID = fingerprint) %>%
+    inner_join(Seatbelts, by = "local_id") -> Seatbelts
+  n2kanalysis::n2k_glmer_poisson(
+    result.datasource.id = sha1(sample(letters)),
+    scheme.id = this_scheme_id,
+    species.group.id = this_species_group_id,
+    location.group.id = this_location_group_id,
+    model.type = "glmer poisson: year",
+    formula = "DriversKilled ~ (1|year)",
+    first.imported.year = 1969L,
+    last.imported.year = 1985L,
+    analysis.date = Sys.time(),
+    data = Seatbelts
+  ) %>%
+    n2kanalysis::fit_model() %>%
+    n2kanalysis::get_result() -> object
+  expect_is(
+    hash <- store_n2kResult(object = object, conn = conn, clean = TRUE),
+    "character"
+  )
+}
